be735e21efeeeed639ecefccd2f3d730cb7d0609
Mark <mark@fatcatapps.com>
2020-05-04 13:30:05 +0900

    Update headerfooter.php

--- headerfooter.php
+++ headerfooter.php
@@ -22,17 +22,27 @@ add_action( 'admin_menu', 'fca_hf_register_admin_page' );
 // UPDATE PLUGIN PAGE
 function fca_hf_admin_page (){
 
-	$header = get_option('fca_hf_header');
-	$footer = get_option('fca_hf_footer');
-	$nonce = wp_create_nonce('fca_update_form');
+	if ( !empty( $_POST ) ) {
 
-	//var_dump($_POST);
+		$nonce = sanitize_text_field( $_POST['nonce_field'] );
 
-	//ob_start(); ?>
+		if ( wp_verify_nonce( $nonce , 'fca_hf_update_form' ) === FALSE ){
+			wp_die( 'Validation failed!' );
+		} else {
+			update_option( 'fca_hf_header', $_POST['fca_hf_header'] );
+			update_option( 'fca_hf_footer', $_POST['fca_hf_footer'] );
+		}
+	}
+
+	$header = wp_unslash( get_option( 'fca_hf_header' ) );
+	$footer = wp_unslash( get_option( 'fca_hf_footer' ) );
+	$nonce = wp_create_nonce( 'fca_update_form' );
+
+	?>
 
 	<h1>HeaderFooter Plugin v0.0.1</h1>
 	<form action="admin.php?page=headerfooter" method="post">
-		<?php wp_nonce_field( 'fca_update_form', 'nonce_field' );?>
+		<?php wp_nonce_field( 'fca_hf_update_form', 'nonce_field' );?>
 		<p>This text will go into your header</p>
 		<textarea name="fca_hf_header"><?php echo $header ?></textarea>
 		<p>This text will go into your footer</p>
@@ -41,32 +51,17 @@ function fca_hf_admin_page (){
 	</form>
 	<?php
 
-	//echo ob_get_clean();
-	//echo($nonce);
-
-	if (!empty($_POST)) {
-
-		//echo($nonce);
-
-		if ( !isset ($nonce) ){
-			echo('No nonce set!');
-		} else {
-			update_option('fca_hf_header', $_POST['fca_hf_header']);
-			update_option('fca_hf_footer', $_POST['fca_hf_footer']);
-		}
-	}
-
 }
 
 // ADD HEADER TEXT TO MAIN PAGE
 function fca_hf_header_output (){
-	echo get_option( 'fca_hf_header' );
+	echo wp_unslash( get_option( 'fca_hf_header' ) );
 }
 add_action( 'wp_head', 'fca_hf_header_output' );
 
 // ADD HEADER TEXT TO MAIN PAGE
 function fca_hf_footer_output (){
-	echo get_option( 'fca_hf_footer' );
+	echo wp_unslash( get_option( 'fca_hf_footer' ) );
 }
 add_action( 'wp_footer', 'fca_hf_footer_output' );
 
